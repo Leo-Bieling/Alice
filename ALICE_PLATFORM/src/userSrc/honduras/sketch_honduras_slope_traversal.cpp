1a1c2104bb78426203a3bc96d87bd04e2434445c
Leo Bieling <mail@leo-bieling.com>
2019-01-30 10:06:07 +0000

    honduras updates

--- ALICE_PLATFORM/src/userSrc/honduras/sketch_honduras_slope_traversal.cpp
+++ ALICE_PLATFORM/src/userSrc/honduras/sketch_honduras_slope_traversal.cpp
@@ -13,18 +13,59 @@
 using namespace zSpace;
 using namespace std;
 
+zGraph rainflow(zMesh _mesh, int _id, vector<zVector> _faceCenters, int _steps)
+{
+	vector<int> tmpConnectedFacesIds;
+	vector<double> tmpConnectedFacesZs;
+	vector<zVector> graphPositions;
+	vector<int> graphEdgeConnections;
+	int lowestId;
+	int tmpFaceId = _id;
 
+	for (int i = 0; i < _steps; i++)
+	{
+		// get connected face Ids
+		_mesh.getConnectedFaces(tmpFaceId, zFaceData, tmpConnectedFacesIds);
+
+		// fill containers for graph
+		graphPositions.push_back(_faceCenters[tmpFaceId]);
+		graphEdgeConnections.push_back(i);
+
+		// store z value of connected face centers in container
+		for (int i = 0; i < tmpConnectedFacesIds.size(); i++)
+			tmpConnectedFacesZs.push_back(_faceCenters[tmpConnectedFacesIds[i]].z);
+
+		// sort z values in container
+		sort(tmpConnectedFacesZs.begin(), tmpConnectedFacesZs.end());
+
+		// assign first/ min value's Id to lowestId integer
+		for (int i = 0; i < tmpConnectedFacesIds.size(); i++)
+			if (tmpConnectedFacesZs[0] == _faceCenters[tmpConnectedFacesIds[i]].z) lowestId = tmpConnectedFacesIds[i];
+
+		// assign lowestID to tmpFaceId for next loop
+		tmpFaceId = lowestId;
+
+		// clear container for next loop
+		tmpConnectedFacesIds.clear();
+		tmpConnectedFacesZs.clear();
+	}
+
+	return zGraph(graphPositions, graphEdgeConnections);
+}
 
 ////////////////////////////////////////////////////////////////////////// GLOBAL VARIABLES ----------------------------------------------------
 ////// --- MODEL OBJECTS ----------------------------------------------------
 zMesh terrain;
+zBufferObject bufferObj;
 vector<zVector> faceCenters;
-
 vector<zVector> seeds;
-vector<zVector> polyPts;
-double rotation;
-int numSteps;
-double distSteps;
+
+//double rotation;
+int numSteps = 20;
+//double distSteps;
+
+int testFaceId1 = 10100;
+int testFaceId2 = 14100;
 
 ////// --- GUI OBJECTS ----------------------------------------------------
 bool smooth = false;
@@ -41,40 +82,21 @@ void setup()
 
 	// import mesh
 	fromOBJ(terrain, "data/turtlingBayPatch.obj");
-	//triangulate	(terrain);
 	getCenters(terrain, zFaceData, faceCenters);
+	setVertexColor(terrain, zColor(0.5, 0.5, 0.5, 1), true);
 	
+	// create buffer
+	bufferObj = zBufferObject(1000000);
+	bufferObj.appendMesh(terrain);
 
 	// get seeds
-	seeds.push_back(faceCenters[10000]);
-
-
-	// put seeds as first point in polyPts
-	polyPts.push_back(seeds[0]);
-
+	seeds.push_back(faceCenters[testFaceId1]);
+	seeds.push_back(faceCenters[testFaceId2]);
 
 	// initialize buttonGroup
 	B = *new ButtonGroup();
 	B.addButton(&smooth, "smooth");
 	B.addButton(&faceNormals, "show face normals");
-
-	////////////////////////////////////////////////////////////////////////// Sliders
-
-	//S = *new SliderGroup();
-
-	//S.addSlider(&background, "background");
-	//S.sliders[0].attachToVariable(&background, 0, 1);
-
-	//////////////////////////////////////////////////////////////////////////// Buttons
-
-	//B = *new ButtonGroup(vec(50, 450, 0));
-
-	//B.addButton(&compute, "compute");
-	//B.buttons[0].attachToVariable(&compute);
-	//B.addButton(&display, "display");
-	//B.buttons[1].attachToVariable(&display);
-
-
 }
 
 void update(int value)
@@ -83,12 +105,13 @@ void update(int value)
 	{
 		// smooth mesh
 		smoothMesh(terrain);
-
-		// flip the bool back to false
+		setVertexColor(terrain, zColor(0.5, 0.5, 0.5, 1), false);
 		smooth = !smooth;
+
+		// clear buffer and amend new mesh
+		bufferObj.clearBufferForRewrite();
+		bufferObj.appendMesh(terrain);
 	}
-
-
 }
 
 ////// ---------------------------------------------------- VIEW  ----------------------------------------------------
@@ -97,28 +120,24 @@ void draw()
 	backGround(0.75);
 	drawGrid(20.0);
 
-	// draw mesh 
-	drawMesh(terrain, false, true, true);
+	// draw from buffer
+	drawLinesFromBuffer(bufferObj);
 
 	// draw face normals
 	if (faceNormals) drawMesh_FaceNormals(terrain, faceCenters, 2.0);
 
 	// draw seed points
 	for (int i = 0; i < seeds.size(); i++)
-	{
-		drawPoint(seeds[i], zColor(1, 0, (1 / 255 * 100), 1), 50);
-	}
-
-
-	// draw buttons
-	B.draw();
+		drawPoint(seeds[i], zColor(1, 0, (1 / 255 * 100), 1), 10);
+	
+	drawGraph(rainflow(terrain, testFaceId1, faceCenters, numSteps), true, true);
+	drawGraph(rainflow(terrain, testFaceId2, faceCenters, numSteps), true, true);
 
 }
 
 ////// ---------------------------------------------------- CONTROLLER  ----------------------------------------------------
 void keyPress(unsigned char k, int xm, int ym)
 {
-
 }
 
 void mousePress(int b, int state, int x, int y)
